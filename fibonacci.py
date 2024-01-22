e6b0101948422da2b52ad62ae8a32241eb71c6d0
Harenibaskar0212 <157300164+Harenibaskar0212@users.noreply.github.com>
2024-01-22 17:21:34 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,16 @@
 
+def fibonacci(n):
+    fib_sequence = [0, 1]
+
+    for _ in range(2, n):
+        next_number = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_number)
+
+    return fib_sequence
+
+# Get input from the user
+n = int(input("Enter the number of Fibonacci numbers to generate: "))
+
+# Generate and print Fibonacci numbers
+result = fibonacci(n)
+print("Fibonacci Sequence:", result)
